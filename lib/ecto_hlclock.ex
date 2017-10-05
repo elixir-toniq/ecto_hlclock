0bd9013a90832cd544ef0afe5fb2a16d08c2c122
Neil Menne <nhm@neilmenne.com>
2017-10-05 11:19:58 -0400

    initialize repository

---
+++ lib/ecto_hlclock.ex
@@ -0,0 +1,18 @@
+defmodule EctoHlclock do
+  @moduledoc """
+  Documentation for EctoHlclock.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoHlclock.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_hlclock_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoHlclockTest do
+  use ExUnit.Case
+  doctest EctoHlclock
+
+  test "greets the world" do
+    assert EctoHlclock.hello() == :world
+  end
+end
