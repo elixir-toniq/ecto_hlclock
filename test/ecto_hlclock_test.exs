0e168a02d8b6fc592ab849f83ae9cf99690feb22
Neil Menne <nhm@neilmenne.com>
2017-10-09 15:30:56 -0400

    coverage for load and dump

--- test/ecto_hlclock_test.exs
+++ test/ecto_hlclock_test.exs
@@ -3,12 +3,21 @@ defmodule EctoHlclockTest do
 
   alias HLClock.Timestamp
 
-  test "cast timestamp" do
-    {:ok, t0} = Timestamp.new(System.os_time(:millisecond), 0, 0)
-    {:ok, t1} = EctoHLClock.cast(t0)
-    assert t0 == t1
+  describe "EctoHLClock" do
+    test "cast/1" do
+      {:ok, t0} = Timestamp.new(System.os_time(:millisecond), 0, 0)
+      {:ok, t1} = EctoHLClock.cast(t0)
+      assert t0 == t1
 
-    {:ok, t2} = t0 |> Timestamp.encode() |> EctoHLClock.cast
-    assert t0 == t2
+      {:ok, t2} = t0 |> Timestamp.encode() |> EctoHLClock.cast
+      assert t0 == t2
+    end
+
+    test "load/1 and dump/1 are symmetric" do
+      {:ok, t0} = Timestamp.new(System.os_time(:millisecond), 0, 0)
+      {:ok, bin} = EctoHLClock.dump(t0)
+      {:ok, t1} = EctoHLClock.load(bin)
+      assert Timestamp.compare(t0, t1) == :eq
+    end
   end
 end
