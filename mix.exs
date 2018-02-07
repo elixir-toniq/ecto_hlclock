f525dd8f12b4fa765b8ab26578bfd4d9b36c622d
Neil Menne <nhm@neilmenne.com>
2018-02-07 18:05:57 -0500

    cleanup for release

--- mix.exs
+++ mix.exs
@@ -6,8 +6,12 @@ defmodule EctoHlclock.Mixfile do
       app: :ecto_hlclock,
       version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      deps: deps()
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Ecto.HLClock",
+      source_url: "https://github.com/toniqsystems/ecto_hlclock"
     ]
   end
 
@@ -17,12 +21,28 @@ defmodule EctoHlclock.Mixfile do
     ]
   end
 
+  defp description do
+    """
+    Ecto type support for storing/retrieving HLC timestamps
+    """
+  end
+
   defp deps do
     [
       {:ecto, "~> 2.2"},
       {:hlclock, "~> 0.1"},
       {:credo, "~> 0.5", only: [:dev, :test]},
-      {:dogma, "~> 0.1", only: [:dev]},
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: :ecto_hlclock,
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Neil Menne", "Chris Keathley"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/toniqsystems/ecto_hlclock"}
     ]
   end
 end
