ba01ce7282af9a7efb47f898c290f4edf995ef7f
Neil Menne <nhm@neilmenne.com>
2018-03-31 16:49:21 -0400

    deps update and necessary credo changes to match

--- test/ecto/hclock_test.exs
+++ test/ecto/hclock_test.exs
@@ -2,21 +2,22 @@ defmodule Ecto.HLClockTest do
   use ExUnit.Case
 
   alias HLClock.Timestamp
+  alias Ecto.HLClock, as: EctoHLC
 
   describe "Ecto.HLClock" do
     test "cast/1" do
       {:ok, t0} = Timestamp.new(System.os_time(:millisecond), 0, 0)
-      {:ok, t1} = Ecto.HLClock.cast(t0)
+      {:ok, t1} = EctoHLC.cast(t0)
       assert t0 == t1
 
-      {:ok, t2} = t0 |> Timestamp.encode() |> Ecto.HLClock.cast()
+      {:ok, t2} = t0 |> Timestamp.encode() |> EctoHLC.cast()
       assert t0 == t2
     end
 
     test "load/1 and dump/1 are symmetric" do
       {:ok, t0} = Timestamp.new(System.os_time(:millisecond), 0, 0)
-      {:ok, bin} = Ecto.HLClock.dump(t0)
-      {:ok, t1} = Ecto.HLClock.load(bin)
+      {:ok, bin} = EctoHLC.dump(t0)
+      {:ok, t1} = EctoHLC.load(bin)
       assert Timestamp.compare(t0, t1) == :eq
     end
   end
