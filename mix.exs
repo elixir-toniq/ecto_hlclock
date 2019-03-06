0e6dff90e910653f57b293041fad06cfe23edd82
Neil Menne <nhm@neilmenne.com>
2019-03-06 16:43:21 -0500

    bump for release

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Ecto.HLClock.Mixfile do
   def project do
     [
       app: :ecto_hlclock,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
